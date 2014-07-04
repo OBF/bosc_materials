54bf3885f5fe7d85e8c3ffc07169fe1899e81c02
Peter Cock <p.j.a.cock@googlemail.com>
2014-07-04 12:08:18 -0400

    Hacks for integrating OpenConf data

--- 2014/program/get_schedule.py
+++ 2014/program/get_schedule.py
@@ -14,6 +14,12 @@ url = "http://www.open-bio.org/wiki/BOSC_2014_Schedule?action=raw"
 txt_file = "schedule.txt"
 tex_file = "schedule.tex"
 tsv_file = "schedule.tsv"
+open_conf = "openconf.tsv"
+
+if not os.path.isfile(open_conf):
+    print("Using OpenConf, Export All Submissions, tick all fields, tab separated")
+    print("Save this file locally as openconf.tsv")
+    open_conf = os.devnull
 
 if not os.path.isfile(txt_file):
     print("Downloading %s --> %s" % (url, txt_file))
@@ -21,7 +27,20 @@ if not os.path.isfile(txt_file):
 else:
     print("Using %s cached as %s" % (url, txt_file))
 
+def load_open_conf(filename):
+    data = {}
+    with open(filename) as handle:
+        header = [t.strip('"').strip() for t in handle.readline().rstrip("\n").split("\t")]
+        #print header
+        title = header.index("TITLE")
+        for line in handle:
+            values = [t.strip('"').strip() for t in line.rstrip("\n").split("\t")]
+            data[values[title].lower()] = dict(zip(header, values))
+    return data
+meta = load_open_conf(open_conf)
+
 def clean(text):
+    text = text.replace("\xe2\x80\x93", "-")
     text = text.strip().replace("_", r"\_").replace("&", r"\&")
     text = text.replace("é", r"{\'e}")
     if text.startswith("'''") and text.endswith("''':"):
@@ -39,6 +58,12 @@ def clean(text):
     elif text.startswith("''") and text.endswith("''"):
         #Double single-quotes for italics
         return r"\textit{%s}" % text[2:-2].strip()
+    elif text.startswith("'''"):
+        #Partial bold
+        bold = text[3:]
+        i = bold.index("'''")
+        bold, rest = bold[:i], bold[i+3:]
+        return r"\textbf{%s} %s" % (bold.strip(), rest.strip())
     else:
         return text
 
@@ -61,6 +86,12 @@ def do_tabs(handle, fields, table):
         #e.g. coffee break
         return
     times, title, name = [clean_latex(t) for t in fields]
+
+    if name.startswith("Suggest a BOF topic"):
+        return
+    if "Pay-your-own-way BOSC dinner" in title:
+        return
+
     if times.startswith("P"):
         poster = times
         day, start, end = "", "", ""
@@ -73,16 +104,43 @@ def do_tabs(handle, fields, table):
             return
         day = {1: "2014/07/11", 2: "2014/07/12"}[table]
 
+    hacks = {"[[BOSC\\_2014\\_Keynote\\_Speakers|Keynote: A History of Bioinformatics (in the Year 2039)]]" : "A History of Bioinformatics (in the Year 20\
+39)",
+"[http://www.open-bio.org/wiki/Main\\_Page Open Bioinformatics Foundation (OBF) update]" : "Open Bioinformatics Foundation (OBF) update",
+"[[Codefest\\_2014 | Codefest 2014 Report]]": "Codefest 2014 Report",
+"| [[BOSC\\_2014\\_Keynote\\_Speakers|Keynote: Biomedical Research as an Open Digital Enterprise]]" : "Biomedical Research as an Open Digital Enterprise",
+}
+    if title in hacks:
+        title = hacks[title]
+
     session = ""
+    if title in ["Lunch", "Announcements", "Registration", "Withdrawn"]:
+        return
+    if title.startswith("Session: ") or \
+       title.startswith("Suggest a BOF") or \
+       title.startswith("Poster Session ") or \
+       title.startswith("Presentation of Student Travel Awards"):
+        return
     if title.startswith("["):
         i = title.index("]")
         session = title[1:i].strip()
         title = title[i+1:].strip()
-    if title.endswith("]") and "[P" in title:
+    if name.endswith("]") and " [P" in name:
         assert not poster
-        title, poster = title.rsplit(None, 1)
+        name, poster = name.strip().rsplit(None, 1)
+        assert poster.startswith("[P") and poster.endswith("]")
+        poster = poster[1:-1]
 
-    handle.write("\t".join([day, start, end, poster, session, title, name]) + "\n")
+    assert title not in ["", "]"], fields
+    try:
+        meta_data = meta[title.lower()]
+    except KeyError:
+        sys.stderr.write("No OpenConf data for: %r\n" % title)
+        meta_data = dict()
+
+    submission_id = meta_data.get("SUBMISSION ID", "?")
+
+    handle.write("\t".join([submission_id, title, name, day, start, end, session, poster]) + "\n")
 
 print("Parsing %s --> %s" % (txt_file, tex_file))
 with open(txt_file) as input:
